9f93d1e73df2960a342305bc0565c09fdd84e259
Ayush-97techyboy <ayushmishra9799@gmail.com>
2026-04-16 17:22:56 +0530

    finalised

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="center">
+      <h1>Sqaure & Circle</h1>
+      <div className="square">
+        <div className="circle"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
